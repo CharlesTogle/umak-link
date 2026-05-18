f51298a51f2ab291dc0a0ecbac33ae045b523a55
CharlesTogle <charles3939togle@gmail.com>
2026-05-18 10:17:25 +0800

    redirect 403 to not allowed

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { UserProvider } from '@/features/auth/contexts/UserContext'
@@ -16,11 +15,9 @@ const queryClient = new QueryClient({
 })
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <UserProvider>
-        <App />
-      </UserProvider>
-    </QueryClientProvider>
-  </StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <UserProvider>
+      <App />
+    </UserProvider>
+  </QueryClientProvider>
 )
